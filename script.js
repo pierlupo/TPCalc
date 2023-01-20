583d3c883dc5cfcb66ce388189dd5953d6612008
Kraimps Pierre <pier_lupo@laposte.net>
2023-01-20 13:19:41 +0100

    calculatrice2

--- script.js
+++ script.js
@@ -1,46 +1,58 @@
+"use strict";
 //Récupération des touches de la calculette et transformation en tableau:
+//let btnCalc = document.querySelectorAll('.btn');
 let btnCalc = [...document.querySelectorAll('.btn')];
 console.log(btnCalc);
-//const btnCalc = document.getElementsByClassName('btn');
+//let btnCalc = document.getElementsByClassName('btn');
 //console.log(btnCalc);
+//let tab1 = Array.from(btnCalc);
+//let tab2 = [...btnCalc];
+//console.log(tab1);
+//console.log(tab2);
 
 //récupération de la data-key attribuée au bouton:
 let listKeycode = btnCalc.map(bouton => bouton.dataset.key);
 console.log(listKeycode);
 
-let ecran = document.querySelector('.ecran');
+let screen = document.querySelector('.ecran');
 //const ecran = document.getElementsByClassName('ecran');
-console.log(ecran);
+console.log(screen);
 
-//évènement sur la touche, transformation du type number en string 
+//évènement sur la touche et transformation du type number en string 
 document.addEventListener('keydown', (e) => {
     const valeur = e.key.toString();
-    ;
+    calculer(valeur);
+    //console.log(valeur);
 });
 
 //évènement au click on met dans une variable valeur au click la dataset définie
 document.addEventListener('click', (e) => {
     const valeur = e.target.dataset.key;
-    console.log(valeur, typeof valeur)
+    calculer(valeur);
+    //on vérifie ici son type
+    //console.log(valeur, typeof valeur)
 })
 
 let calculer = (valeur) => {
     if(listKeycode.includes(valeur)){
         switch(valeur) {
             case '46':
-                ecran.textContent = "";
+                screen.textContent = "";
                 break;
             case '13':
-                const calcul = eval(ecran.textContent);
-                ecran.textContent = calcul;
+                const calcul = eval(screen.textContent);
+                screen.textContent = calcul;
+                console.log(calcul);
                 break;
             default:
             const indexKeycode = listKeycode.indexOf(valeur);
             const bouton = btnCalc[indexKeycode];
-            ecran.textContent += bouton.innerHTML;
+            screen.textContent += bouton.innerHTML;
         }
     }
 }
 
-
+window.addEventListener('error', (e)=> {
+    alert('Une erreur est survenue : '+ e.message);
+})
 
